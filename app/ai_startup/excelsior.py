ab3331628242a3ddd1ef0851811186d6c85c5939
ThyagoSCoelho <thystockler@gmail.com>
2023-10-02 00:23:29 -0300

    feat(excelsior): Refatorando download e carregamento da IA

---
+++ app/ai_startup/excelsior.py
@@ -0,0 +1,21 @@
+import os
+import string
+from transformers import AutoProcessor, BarkModel
+
+os.environ["SUNO_OFFLOAD_CPU"] = "True"
+os.environ["SUNO_USE_SMALL_MODELS"] = "True"
+
+class Excelsior:
+    
+    def __init__(self, model_path=string):
+        self.model_path = model_path
+        print(self.model_path)
+    
+    def initialize(self):
+        self.processor = AutoProcessor.from_pretrained(pretrained_model_name_or_path=self.model_path)
+        self.model = BarkModel.from_pretrained(pretrained_model_name_or_path=self.model_path)
+        #     config=config,
+        #     local_files_only=True,
+        #     
+        # ).to(device)
+        return self.processor, self.model
