dd62479390ceffb538b75c2a488a00ba1e63f9f3
ThyagoSCoelho <thystockler@gmail.com>
2023-10-02 00:52:34 -0300

    chore(initializer_excelsior): Refatoração da classe inicializadora

--- app/ai_startup/initializer_excelsior.py
+++ app/ai_startup/initializer_excelsior.py
@@ -5,13 +5,14 @@ from transformers import AutoProcessor, BarkModel
 os.environ["SUNO_OFFLOAD_CPU"] = "True"
 os.environ["SUNO_USE_SMALL_MODELS"] = "True"
 
-class Excelsior:
+class InitializerExcelsior:
     
     def __init__(self, model_path=string):
         self.model_path = model_path
         print(self.model_path)
     
     def initialize(self):
+        # config = BarkConfig.from_json_file(model_pretreined + "/config.json")
         self.processor = AutoProcessor.from_pretrained(pretrained_model_name_or_path=self.model_path)
         self.model = BarkModel.from_pretrained(pretrained_model_name_or_path=self.model_path)
         #     config=config,
